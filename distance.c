7f13ab24a91c7234f666e0b89e0f0c933b73eeb3
ENG19CS0224 <76087380+ENG19CS0224@users.noreply.github.com>
2021-02-11 14:14:44 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -2,32 +2,35 @@
 #include<stdio.h>
 #include<math.h>
 
-int a1,b1,a2,b2;
-float d;
+int pointA()
+{
+   float a;
+   printf("Enter the values a:");
+   scanf("%f",&a);
+   return a;
+}
+int pointB()
+{
+  float b;
+  printf("Enter the values b:");
+  scanf("%f",&b);
+   return b;
+}
+void distance(float a1,float b1,float a2,float b2)
+{
+  float d;
+  d = (sqrt(pow((a1-a2),2)+pow((b1-b2),2)));
+  printf("Distance between point1-(%d,%d) and point2-(%d,%d) is %f",a1,b1,a2,b2,d);
 
-float input()
-{
-    printf("Enter the point1-(a1,b1) and point2-(a2,b2):");
-    scanf("%d%d%d%d",&a1,&b1,&a2,&b2);
-    return 0;
 }
-float distance()
-{
-  float s;
-  s= (pow((a1-a2),2)+pow((b1-b2),2));
-  d = sqrt(s);
-  return 0;
-}
-void output()
-{
-    printf("Distance between point1-(%d,%d) and point2-(%d,%d) is %f",a1,b1,a2,b2,d);
-  return 0;
-}
+
 int main()
 {
-
-    input();
-    distance();
-    output();
-    return 0;
+  float a1,b1,a2,b2,d;
+  a1 = pointA();
+  b1 = pointB();
+  a2 = pointA();
+  b2 = pointB();
+  distance(a1,b1,a2,b2);
+  return 0;
 }
